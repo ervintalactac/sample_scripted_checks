0f15b65d40a87daf2ae2849cb714f9268994adde
ervintalactac <ervin.talactac@apicasystem.com>
2023-01-31 10:50:56 -0800

    Add files via upload
    test

--- Apica_connection_check.py
+++ Apica_connection_check.py
@@ -1,35 +1,69 @@
-import time
-import sys
-from snowflake.connector import connect
-import logging
-import json
+#!/usr/bin/env python 
+import sys 
+import snowflake.connector 
+from snowflake.connector import connect 
+import logging 
+import json 
 import pandas as pd
+import time
 
-USER = 'SVC_SNOWDEV_EDPDEV_APICA'
-PASSWORD = 'sn0wflak3_SF@apicadev9'
-ACCOUNT = 'deltadentalins-dev'
+USER = "SVC_SNOWDEV_EDPDEV_APICA"
+PASSWORD = "sn0wflak3_SF@apicadev9"
+ACCOUNT = "deltadentalins-dev"
+DB = "edpdev"
+WAREHOUSE = "wh_edpdev_rpt"
 
-start = time.time()
+conn = connect(
+    user=USER,
+    password=PASSWORD,
+    account=ACCOUNT,
+    database=DB,
+    warehouse=WAREHOUSE,
+    session_parameters={
+        'QUERY_TAG': 'APICA',
+    }
+    )
 
 
+sql1 = "SELECT CURRENT_TIMESTAMP() AS EVENT_TIMESTAMP, CURRENT_USER AS USER_NAME"
+
+sql2 = "select * from table(information_schema.login_history_by_user(user_name => 'SVC_SNOWDEV_EDPDEV_APICA', result_limit => 1)) order by event_timestamp"
+
+startTime = time.time()
+
+cursor = conn.cursor()
+cursor.execute(sql2)
+#first_row = cur.fetchone()
+df = cursor.fetch_pandas_all()
+#print(df)
+#print(first_row)
+
+
+return_code = 0
+current_time = str(df.EVENT_TIMESTAMP[0])
+current_user = df.USER_NAME[0]
+is_success = df.IS_SUCCESS[0]
+message = ("IS_LOGIN_SUCCESSFUL: " + is_success ) 
+value = df.ERROR_CODE[0]
+
+endTime = time.time()
+
+if value is None:
+    value_code = 200
+else:
+    value_code = value
+    
 #json = df.to_json()
 #print(json)
-end = time.time()
 
 json_return = {
-  'returncode': 0,
-  'start_time': start,
-  'value': 0,
-  'end_time': end,
-  'message': 'HTTP Call completed with status OK',
-  'metrics': {
-    'duration': 0,
-    'content_size': 1256,
-    'header_count': 11,
-    'http_status': 200
-  }
+    "returncode": return_code,
+    "start_time": startTime,
+    "end_time": endTime,
+    "current_user": current_user,
+    "message": message,
+    "value": value_code
 }
-
 print(json.dumps(json_return))
 cursor.close()
-#print('Closed Snowflake connection')
+#print("Closed Snowflake connection")
