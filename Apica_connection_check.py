97bae438aa2e8d5b0fb6f654be8e7079502f4efa
ervintalactac <ervin.talactac@apicasystem.com>
2023-02-06 08:49:02 -0800

    Add files via upload
    redacted version

--- Apica_connection_check.py
+++ Apica_connection_check.py
@@ -1,69 +1,49 @@
-#!/usr/bin/env python 
-import sys 
-import snowflake.connector 
-from snowflake.connector import connect 
-import logging 
-import json 
+# this is our basic python connection test script from the docs
+#!/usr/bin/env python
+import sys
+from snowflake.connector import connect
+import logging
+import json
 import pandas as pd
-import time
 
-USER = "SVC_SNOWDEV_EDPDEV_APICA"
-PASSWORD = "sn0wflak3_SF@apicadev9"
-ACCOUNT = "deltadentalins-dev"
-DB = "edpdev"
-WAREHOUSE = "wh_edpdev_rpt"
+USER = ""
+PASSWORD = ""
+ACCOUNT = ""
 
 conn = connect(
     user=USER,
     password=PASSWORD,
     account=ACCOUNT,
-    database=DB,
-    warehouse=WAREHOUSE,
+    #warehouse=WAREHOUSE, --optional if you want to use WH for running other queries as part of script
     session_parameters={
-        'QUERY_TAG': 'APICA'
+        'QUERY_TAG': 'APICA',
     }
-)
+    )
 
 
-sql1 = "SELECT CURRENT_TIMESTAMP() AS EVENT_TIMESTAMP, CURRENT_USER AS USER_NAME"
-
-sql2 = "select * from table(information_schema.login_history_by_user(user_name => 'SVC_SNOWDEV_EDPDEV_APICA', result_limit => 1)) order by event_timestamp"
-
-startTime = time.time()
+sql1 = "SELECT CURRENT_TIMESTAMP() AS CURRENT_TIME, CURRENT_USER AS CONNECTED_USER"
 
 cursor = conn.cursor()
-cursor.execute(sql2)
-#first_row = cur.fetchone()
+cursor.execute(sql1)
+
 df = cursor.fetch_pandas_all()
 #print(df)
-#print(first_row)
-cursor.close()
 
+current_time = df.CURRENT_TIME[0]
+current_user = df.CONNECTED_USER[0]
 
-return_code = 0
-current_time = str(df.EVENT_TIMESTAMP[0])
-current_user = df.USER_NAME[0]
-is_success = df.IS_SUCCESS[0]
-message = ("IS_LOGIN_SUCCESSFUL: " + is_success ) 
-value = df.ERROR_CODE[0]
-
-endTime = time.time()
-
-if value is None:
-    value_code = 200
-else:
-    value_code = value
-    
 #json = df.to_json()
 #print(json)
 
 json_return = {
-    "returncode": return_code,
-    "start_time": startTime,
-    "end_time": endTime,
+    "returncode": 0,
+    "current_time": current_time,
     "current_user": current_user,
-    "message": message,
-    "value": value_code
+    "message": "URL call returned status code: " + str(response.status_code),
+    "unit": "ms",
+    "value": response.status.code,
+
 }
 print(json.dumps(json_return))
+cursor.close()
 #print("Closed Snowflake connection")
